f678e6249a2b91c2553b1a9ee85bcf67c3be3bdf
saddzoe <73001138+saddzoe@users.noreply.github.com>
2021-01-21 22:22:36 -0500

    Update javasriptProject1.js

--- javasriptProject1.js
+++ javasriptProject1.js
@@ -1,2 +1,10 @@
-# javascriptProject1.js
+function palindrome(str) {
+  let originalStr = str.replace(/\W+|_/g, " ").toLowerCase();
+  let backwardWord = originalStr.split("").reverse().join("");
+  
+  if (originalStr != backwardWord) {
+    return false;
+  }
+  return true;
+}
 
